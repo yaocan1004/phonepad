28d1038804faaa49761b8c47a17952ce6df9a697
Dawei Zhuang <dawei.zhuang@antra.com>
2019-11-27 10:01:10 -0500

    Initial commit

---
+++ src/test/java/com/antra/phonepad/combination/CombinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antra.phonepad.combination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
